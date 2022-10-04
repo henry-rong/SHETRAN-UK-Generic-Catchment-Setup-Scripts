7f900e9a918736479fc70bd4c18202361a3076cb
Ben Smith <dr.ben.smith@outlook.com>
2022-10-04 11:28:02 +0100

    Update SHETRAN_GB_Master_Setup.py
    Testing online commit method for pushing

--- SHETRAN_GB_Master_Setup.py
+++ SHETRAN_GB_Master_Setup.py
@@ -10,6 +10,7 @@
 # that makes it super complex). This script often uses 'Try', which means that sometimes errors are hidden.
 # Look to change that over time.
 #
+#
 # --- USER INSTRUCTIONS:
 # The user should only have to edit:
 #  --- Set File Paths
@@ -17,14 +18,25 @@
 # All other code should be fixed. Make sure that you update the correct parts of the dictionaries.
 # 'process_single_catchment["single"]' controls 'multiprocessing["process_multiple_catchments"]'
 #
+#
 # --- MASKS:
 # Masks should be .asc
 # Mask paths used in multiprocessing will be:
-#   mask_folder_prefix + sim_mask_path + sim_name + Mask.asc.
+#   mask_folder_prefix + sim_mask_path + sim_name + Mask.txt
 #   Leave sim_mask_path in the csv blank if there are no additional bits to add in here.
 #   mask_folder_prefix can be changed to "" and the prefixed folder specified in the CSV
 #    instead if more specific naming convention is needed.
 # Masks MUST align with the nearest 1000m! Else your climate data will be blank.
+# Masks MUST NOT have isolated cells / diagonally connected cells. This will stop the
+# SHETRAN Prepare.exe script from setting up. Remove these manually (and check any
+# corresponding cell maps.
+#
+# --- Northern Ireland:
+# NI datasets are not fully included into the setup and so are processed differently. They
+# do not have gridded climate data, instead, this is available in processed form for a
+# selection of catchments (this was provided by Helen He at UEA). These only run until the
+# end of 2010.
+#
 #
 # TODO
 # - Update the climate input data to most current period.
@@ -44,7 +56,8 @@
 # --- Load in Packages ----------------------------------------
 import numpy as np
 import time
-import SHETRAN_GB_Master_Setup_Functions as sf
+import SHETRAN_GB_Master_Setup_Functions as SF
+import pandas as pd  # for reading CSVs
 
 # -------------------------------------------------------------
 # --- USER INPUTS ---------------------------------------------
@@ -53,34 +66,36 @@ import SHETRAN_GB_Master_Setup_Functions as sf
 # --- Set File Paths -------------------------------------------
 
 # Climate input folders:
-create_climate_data = True
+create_climate_data = False
 rainfall_input_folder = 'I:/CEH-GEAR downloads/'
 temperature_input_folder = 'I:/CHESS_T/'
 PET_input_folder = 'I:/CHESS/'
 
 # Model period: 'yyyy-mm-dd'
-start_time = '2000-01-01'
-end_time = '2015-12-30'
+start_time = '1980-01-01'
+end_time = '2010-12-31'
 
 # Static Input Data Folder:
 raw_input_folder = "I:/SHETRAN_GB_2021/inputs/Raw ASCII inputs for SHETRAN UK/"
 
 # --- Set Processing Methods -----------------------------------
-PYRAMID = 'C:/Users/nbs65/Newcastle University/PYRAMID - General/WP3/02 SHETRAN Simulations/'
+# PYRAMID = 'C:/Users/nbs65/Newcastle University/PYRAMID - General/WP3/02 SHETRAN Simulations/'
 process_single_catchment = dict(
-    single=True,
-    simulation_name='Tyne_at_Newcastle',
-    mask_path=PYRAMID + "09_Tyne_at_Newcastle_03/Tyne_at_Newcastle_SetupMask.asc",
-    output_folder=PYRAMID + "09_Tyne_at_Newcastle_05/")
+    single=False,
+    simulation_name='203050',
+    mask_path="I:/SHETRAN_GB_2021/inputs/1kmBngMasks_Processed/203050_Mask.txt",
+    output_folder="I:/SHETRAN_GB_2021/historical_220601_GB_APM_Additions/203050/")
 
 # Choose Single / Multiprocessing:
 multiprocessing = dict(
     process_multiple_catchments=not process_single_catchment["single"],
-    simulation_list_csv='I:/SHETRAN_GB_2021/creation_test/Simulation_Setup_List.csv',
-    mask_folder_prefix='I:/SHETRAN_GB_2021/creation_test/masks/',
-    output_folder_prefix='I:/SHETRAN_GB_2021/creation_test/',
-    use_multiprocessing=True, n_processes=2,
-    use_groups=True, group=1)  # not used when use_groups == False.[True, False][1]
+    simulation_list_csv='C:/Users/nbs65/OneDrive - Newcastle University/Python Code/SHETRAN_generic_catchment_setup/Simulation_Setup_List.csv',
+    mask_folder_prefix='I:/SHETRAN_GB_2021/inputs/1kmBngMasks_Processed/',
+    output_folder_prefix='I:/SHETRAN_GB_2021/historical_220601_GB_APM_Additions/',
+    use_multiprocessing=False,  # May only work on Blades?
+    n_processes=9,  # For use on the blades
+    use_groups=True,  # [True, False][1]
+    group="2")  # String. Not used when use_groups == False.
 
 # -------------------------------------------------------------
 # --- CALL FUNCTIONS FOR SETUP --------------------------------
@@ -90,14 +105,15 @@ if __name__ == "__main__":
 
     if process_single_catchment["single"] == multiprocessing["process_multiple_catchments"]:
         print("WARNING - SINGLE AND MULTIPLE PROCESSES SELECTED. Check process_single_catchment & multiprocessing.")
+        pass
 
     # --- Import the Static Dataset -------------------------------
-    static_data = sf.read_static_asc_csv(raw_input_folder)
+    static_data = SF.read_static_asc_csv(raw_input_folder)
 
     # --- Call Functions to Process a Single Catchment ------------
     if process_single_catchment["single"]:
         print("Processing single catchment...")
-        sf.process_catchment(
+        SF.process_catchment(
             catch=process_single_catchment["simulation_name"], mask_path=process_single_catchment["mask_path"],
             simulation_startime=start_time, simulation_endtime=end_time,
             output_subfolder=process_single_catchment["output_folder"], static_inputs=static_data,
@@ -109,30 +125,29 @@ if __name__ == "__main__":
     if multiprocessing["process_multiple_catchments"]:
         print("Processing multiple catchments...")
 
-        # Set up a list of catchments to process:
-        simulation_names = []
-        simulation_masks = []
-        output_folders = []
-
         # Read a list of simulation names to process:
-        with open(multiprocessing["simulation_list_csv"], 'r') as fh:
-            # Read a line from the csv:
-            fh.readline()
-            for line in fh:
-                sim_name, sim_mask_path, _, process_group, sim_out_path = line.rstrip().split(',')
-                # Check whether the simulation is in the group we're processing:
-                if multiprocessing["use_groups"]:
-                    if int(process_group) != multiprocessing["group"]:
-                        continue
-                # If so then append it to the list:
-                simulation_names.append(sim_name)
-                simulation_masks.append(multiprocessing["mask_folder_prefix"] + sim_mask_path + sim_name + "_Mask.asc")
-                output_folders.append(multiprocessing["output_folder_prefix"] + sim_out_path + sim_name + "/")
+        catchments_csv = pd.read_csv(multiprocessing["simulation_list_csv"], keep_default_na=False)
+
+        # Check whether the simulation is in the group we're processing:
+        if multiprocessing["use_groups"]:
+            catchments_csv = catchments_csv[catchments_csv["Group"] == multiprocessing["group"]]
+
+        simulation_names = list([str(c) for c in catchments_csv["Simulation_Name"]])
+
+        simulation_masks = [multiprocessing["mask_folder_prefix"] +
+                            str(catchments_csv["Additional_Mask_Path"][x]) +
+                            str(catchments_csv["Simulation_Name"][x]) + "_Mask.txt"
+                            for x in catchments_csv["Simulation_Name"].index]
+
+        output_folders = [multiprocessing["output_folder_prefix"] +
+                          str(catchments_csv["Additional_Output_Path"][x]) +
+                          str(catchments_csv["Simulation_Name"][x]) + "/"
+                          for x in catchments_csv.index]
 
         if multiprocessing["use_multiprocessing"]:
             print("Using multi-processing...")
 
-            sf.process_mp(mp_catchments=simulation_names, mp_mask_folders=simulation_masks,
+            SF.process_mp(mp_catchments=simulation_names, mp_mask_folders=simulation_masks,
                           mp_output_folders=output_folders, mp_simulation_startime=start_time,
                           mp_simulation_endtime=end_time, mp_static_inputs=static_data,
                           mp_produce_climate=create_climate_data, mp_prcp_input_folder=rainfall_input_folder,
@@ -140,13 +155,18 @@ if __name__ == "__main__":
                           num_processes=multiprocessing["n_processes"])
 
         else:
-            print("Using single processing...")
+            print("Using single processor...")
             for c in np.arange(0, len(simulation_names)):
-                sf.process_catchment(catch=simulation_names[c], mask_path=simulation_masks[c],
-                                     simulation_startime=start_time, simulation_endtime=end_time,
-                                     output_subfolder=output_folders[c], static_inputs=static_data,
-                                     produce_climate=create_climate_data, prcp_input_folder=rainfall_input_folder,
-                                     tas_input_folder=temperature_input_folder, pet_input_folder=PET_input_folder)
+                # print(simulation_masks[c])
+                SF.process_catchment(catch=simulation_names[c],
+                                     mask_path=simulation_masks[c],
+                                     simulation_startime=start_time,
+                                     simulation_endtime=end_time,
+                                     output_subfolder=output_folders[c],
+                                     static_inputs=static_data,
+                                     produce_climate=create_climate_data,
+                                     prcp_input_folder=rainfall_input_folder,
+                                     tas_input_folder=temperature_input_folder,
+                                     pet_input_folder=PET_input_folder)
                 time.sleep(1)
     print("Finished Processing Catchments")
-
